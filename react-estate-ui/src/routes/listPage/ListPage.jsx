43aaa9d77043539cbe591ab2a536d11145c6d2bf
Adarsh Rawat <00adarshrawat00@gmail.com>
2024-07-13 17:38:35 +0530

    design single page

--- react-estate-ui/src/routes/listPage/ListPage.jsx
+++ react-estate-ui/src/routes/listPage/ListPage.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { listData } from '../../lib/dummydata'
+import './listPage.scss';
+import Filter from '../../components/filter/Filter';
+import Card from '../../components/card/Card';
+import Map from '../../components/map/Map';
+import "leaflet/dist/leaflet.css";
 
 function ListPage() {
+
+  const data = listData;
+
   return (
-    <div>ListPage</div>
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter/>
+          {data.map((item)=>(
+            <Card key={item} item={item}/>
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        <Map items={data}/>
+      </div>
+    </div>
   ) 
 }
 
